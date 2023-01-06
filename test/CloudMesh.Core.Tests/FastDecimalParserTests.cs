4bce1682828f001797a2401e3f222fa8be49cb11
Jessie Wadman <jessie@wadmanit.se>
2023-01-06 20:34:50 +0100

    Added missing files

---
+++ test/CloudMesh.Core.Tests/FastDecimalParserTests.cs
@@ -0,0 +1,50 @@
+using System.Globalization;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Running;
+using Xunit.Abstractions;
+
+namespace CloudMesh.Core.Tests;
+
+public class FastDecimalParserTests
+{
+    private readonly ITestOutputHelper _output;
+
+    public FastDecimalParserTests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+    
+    [Fact]
+    public void Benchmark()
+    {
+        var logger = new AccumulationLogger();
+
+        var config = ManualConfig.Create(DefaultConfig.Instance)
+            .AddLogger(logger)
+            .WithOptions(ConfigOptions.DisableOptimizationsValidator);
+
+        BenchmarkRunner.Run<FastDecimalBenchmarks>(config);
+
+        // write benchmark summary
+        _output.WriteLine(logger.GetLog());
+    }
+
+    public class FastDecimalBenchmarks
+    {
+        const string decimalString = "123490123456.1235412";
+        
+        [Benchmark]
+        public void FastDecimalParsing()
+        {
+            OptimizationHelpers.FastTryParseDecimal(decimalString, DecimalSeparators.ISO, out var parsedValue);
+        }
+        
+        [Benchmark]
+        public void DefaultDecimalParsing()
+        {
+            decimal.TryParse(decimalString, CultureInfo.InvariantCulture, out var parsedValue);
+        }
+    }
+}
